66f4f35fcd6209471b4cb5d7c1ab724ad48b57e3
rampremverma108 <vermarp221@gmail.com>
2026-06-01 12:19:14 +0530

    logic update

--- loops/reverseNumber.java
+++ loops/reverseNumber.java
@@ -1,5 +1,17 @@
+import java.util.*;
+
+
 public class reverseNumber{
     public static void main(String[] args){
-        
+        System.out.print("Input number to get reverse number : ");
+     Scanner sc = new Scanner(System.in);
+     int number = sc.nextInt();
+
+    while(number > 0){
+    int lastdigit = number%10;
+    System.out.print(lastdigit);
+    number /=10;
+    }
+    System.out.println();
     }
 }
