301a19792c830c2c7ef07991013b8e6c65b3a4d3
rampremverma108 <vermarp221@gmail.com>
2026-06-03 22:12:05 +0530

    modified program

--- patterns/starPyramid.java
+++ patterns/starPyramid.java
@@ -1,9 +1,8 @@
 public class starPyramid{
     public static void main(String args[]){
-        int row =5;
-        for(int i =1; i<=row; i++){
+      
+        for(int i =1; i<=5; i++){
             for(int j=1; j<=i; j++){
-        
          System.out.print("*");
             }
             System.out.println();
