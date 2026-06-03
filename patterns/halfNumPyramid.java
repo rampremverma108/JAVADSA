654b38a463271ed130a6b056d8bca1acb1a17c5e
rampremverma108 <vermarp221@gmail.com>
2026-06-03 23:21:53 +0530

    added charPattern and halfPyramid

---
+++ patterns/halfNumPyramid.java
@@ -0,0 +1,10 @@
+public class numberPyramid{
+    public static void main(String args[]){
+        for(int line= 1; line<=5; line++){
+            for(int n =1; n<=line; n++){
+                System.out.print(n);
+            }
+            System.out.println();
+        }
+    }
+}
