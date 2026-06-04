564ac5a30cc956ed36ba21fcd5147bbdb3630373
rampremverma108 <vermarp221@gmail.com>
2026-06-04 22:46:03 +0530

    floyd pattern code add

---
+++ patterns/floydTringle.java
@@ -0,0 +1,12 @@
+public class invertedhalfPyramid{
+    public static void main(String args[]){
+        int number = 1;
+        for(int line =1; line<=5; line++){
+            for(int count=1; count<= line; count++){
+                System.out.print(number+" ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
