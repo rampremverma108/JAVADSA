4d3f861c3b3ad4cb0af3f187246816f6ec0c2243
rampremverma108 <vermarp221@gmail.com>
2026-06-07 09:01:35 +0530

    optimise primeNumber

---
+++ functionMathods/primeNumber.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class primeNumber{
+
+
+    // public static boolean isPrime(int n){
+    //     boolean isPrime = true;
+    //     for(int i=2; i <=Math.sqrt(n); i++){
+    //         if(n % i ==0){
+    //             isPrime =false;
+    //         }
+    //     }
+    //     return isPrime;
+    // }
+
+public static boolean isPrime(int n){
+    if (n == 2){
+        return false;
+    }
+    for (int i = 2; i<= Math.sqrt(n); i++){
+        if ( n% i==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number to check is prime : ");
+        int n = sc.nextInt();
+       System.out.println(isPrime(n));
+    }
+}
