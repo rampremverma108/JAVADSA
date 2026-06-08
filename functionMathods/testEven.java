a7109f2712b8839419b00d0bfc92bf56ba2523b8
rampremverma108 <vermarp221@gmail.com>
2026-06-08 23:18:17 +0530

    check np. even or not

---
+++ functionMathods/testEven.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class testEven{
+    public static boolean isEven(int n){
+        boolean isEven = true;
+        if(n%2!=0){
+            return false;
+        }
+        return isEven;
+    }
+    public static void main(String sdfd[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number to check even or not : ");
+        int n = sc.nextInt();
+        System.out.print("Result: "+ isEven(n));
+        sc.close();
+    }
+}
