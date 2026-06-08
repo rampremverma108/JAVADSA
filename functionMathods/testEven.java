5dba44f93a9e22d526cff2d29ff5d580f9bc5272
rampremverma108 <vermarp221@gmail.com>
2026-06-08 23:24:28 +0530

    check no. even or odd and enhance optimization

--- functionMathods/testEven.java
+++ functionMathods/testEven.java
@@ -3,10 +3,11 @@ import java.util.*;
 public class testEven{
     public static boolean isEven(int n){
         boolean isEven = true;
-        if(n%2!=0){
-            return false;
-        }
-        return isEven;
+        // if(n%2!=0){
+        //     return false;
+        // }
+        // return isEven;
+        return (n % 2 ==0);  //optimised code
     }
     public static void main(String sdfd[]){
         Scanner sc = new Scanner(System.in);
