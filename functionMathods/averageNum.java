281ba4050d933b5c538561ff154c6e37aa4fb106
rampremverma108 <vermarp221@gmail.com>
2026-06-08 22:45:57 +0530

    average of three no. by user

---
+++ functionMathods/averageNum.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class averageNum{
+public static float avg(float n1, float n2, float n3){
+    float sum = n1+ n2 +n3;
+    float average = sum/3;
+    return average;
+}
+
+public static void main(String args[]){
+    System.out.print("Enter 3 numbers to get an Average.\nEnter 1st No : ");
+    Scanner sc = new Scanner(System.in);
+    float n1 = sc.nextFloat();
+    System.out.print("Enter 2nd No : ");
+    float n2 = sc.nextFloat();
+    System.out.print("Enter 3rd No : ");
+    float n3 = sc.nextFloat();
+    System.out.println(avg(n1,n2,n3));
+    
+}
+}
