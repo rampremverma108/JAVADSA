67ecfacad5b5a26fff1ea97cd72ea1622f1d007c
rampremverma108 <vermarp221@gmail.com>
2026-06-10 17:31:41 +0530

    change condition with concept of matrix

--- patterns/binaryTringle.java
+++ patterns/binaryTringle.java
@@ -2,11 +2,12 @@ public class binaryTringle{
     public static void main(String args[]){
         for(int line =1; line<=5; line++){
             for (int j = 1; j <= line; j++) {
-            if (j %2 !=0){
-                System.out.print("1");
-            }else{
-                System.out.print("0");
-            }}
+                if((line+j)%2 == 0){
+                   System.out.print("1"); 
+                }else{
+                    System.out.print("0");
+                }
+           }
             System.out.println();
         }
     }
