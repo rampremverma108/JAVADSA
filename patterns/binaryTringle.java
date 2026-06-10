c8391a102123005336e910439bd7541f776188aa
rampremverma108 <vermarp221@gmail.com>
2026-06-10 17:35:00 +0530

    solve line change problem

--- patterns/binaryTringle.java
+++ patterns/binaryTringle.java
@@ -2,13 +2,18 @@ public class binaryTringle{
     public static void main(String args[]){
         for(int line =1; line<=5; line++){
             for (int j = 1; j <= line; j++) {
+            // if (j %2 !=0){
+            //     System.out.print("1");
+            // }else{
+            //     System.out.print("0");
+            // }
                 if((line+j)%2 == 0){
                    System.out.print("1"); 
                 }else{
                     System.out.print("0");
                 }
            }
-            System.out.println();
+           System.out.println();
         }
-    }
+        }
 }
