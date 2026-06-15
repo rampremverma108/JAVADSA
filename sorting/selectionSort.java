7b69e9357a47795177d8cbd9b20630d360879f89
rampremverma108 <vermarp221@gmail.com>
2026-06-15 23:04:43 +0530

    selection sort technique

---
+++ sorting/selectionSort.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Sorting{
+     public static void selectionSort(int arr[]){
+        for(int i =0; i<arr.length-1; i++){
+            int minPos =i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[minPos] > arr[j]){
+                    minPos =j;
+                }
+            }
+            //swap
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] =temp;
+        }
+     }
+
+ public static void main(String args[]){
+        int arr[] = {5,4,1,3,2};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
