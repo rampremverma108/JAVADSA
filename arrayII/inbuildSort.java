23df543ddfc708252bdf45b5e8202d1bf04e4945
rampremverma108 <vermarp221@gmail.com>
2026-06-16 22:41:20 +0530

    inbuild fn for sort

---
+++ arrayII/inbuildSort.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Sort{
+        public static void main(String args[]){
+        int arr[] = {5,4,1,3,2};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        }
+    }
