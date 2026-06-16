1f68b535a88113e82839b3726ce1fb281b464093
rampremverma108 <vermarp221@gmail.com>
2026-06-16 23:07:00 +0530

    reverserOrder() function please

--- arrayII/inbuildSort.java
+++ arrayII/inbuildSort.java
@@ -1,13 +1,22 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Sort{
+
+    public static void reverseOrder(Integer nu[]){
+       Arrays.sort(nu, Collections.reverseOrder());
+        System.out.println(Arrays.toString(nu));
+    }
+
+
         public static void main(String args[]){
         int arr[] = {5,4,1,3,2};
-            //Arrays.sort(arr);
-
+        Integer nu[] = {2,8,4,6,3};
+        //Arrays.sort(arr);
         //agr hame kuch numbers tk he sort krne hai to starting and ending num de kr kr skte hain
 
         Arrays.sort(arr, 0, 3);
-        System.out.println(Arrays.toString(arr));
+        System.out.println("Partially sorted arr (0 to 2): " + Arrays.toString(arr));
+       reverseOrder(nu);
         }
     }
