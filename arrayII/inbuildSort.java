bbe8476bf87f33fe24e8759323d1d5bbe2aba1e5
rampremverma108 <vermarp221@gmail.com>
2026-06-16 22:48:38 +0530

    add index value till to sort

--- arrayII/inbuildSort.java
+++ arrayII/inbuildSort.java
@@ -3,7 +3,11 @@ import java.util.Arrays;
 public class Sort{
         public static void main(String args[]){
         int arr[] = {5,4,1,3,2};
-        Arrays.sort(arr);
+            //Arrays.sort(arr);
+
+        //agr hame kuch numbers tk he sort krne hai to starting and ending num de kr kr skte hain
+
+        Arrays.sort(arr, 0, 3);
         System.out.println(Arrays.toString(arr));
         }
     }
