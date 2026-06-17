3e1d4e081c546831e31eb084b84283a8155eb91f
rampremverma108 <vermarp221@gmail.com>
2026-06-17 22:33:57 +0530

    Add sorting files moved from arrayII

---
+++ sorting/inbuildSort.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Sort{
+
+    public static void reverseOrder(Integer nu[]){
+       Arrays.sort(nu, Collections.reverseOrder());
+        System.out.println(Arrays.toString(nu));
+    }
+
+    public static int compare(int a, int b){
+        // a < b -ve
+        // a == b 0 
+        // a > b +ve
+        return a-b;
+    }
+
+
+        public static void main(String args[]){
+        int arr[] = {5,4,1,3,2};
+        Integer nu[] = {2,8,4,6,3};
+        //Arrays.sort(arr);
+        //agr hame kuch numbers tk he sort krne hai to starting and ending num de kr kr skte hain
+
+        Arrays.sort(arr, 0, 3);
+        System.out.println("Partially sorted arr (0 to 2): " + Arrays.toString(arr));
+       reverseOrder(nu);
+       System.out.println(compare(5, 6));
+        }
+    }
