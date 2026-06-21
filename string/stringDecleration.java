4b2203e85d322549bb431a15b69d4092f30e96ff
rampremverma108 <vermarp221@gmail.com>
2026-06-21 23:42:11 +0530

    string decleration concept

---
+++ string/stringDecleration.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class StringVacob{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your Name: ");
+       // String name =sc.next(); //for one word only
+       String name = sc.nextLine(); //for sentance
+        String str = "You are Good Person";
+        String str2 = new String("Be Healthy and Enjoy your Life");
+        System.out.println(str);
+        System.out.println(str2);
+    }
+    }
