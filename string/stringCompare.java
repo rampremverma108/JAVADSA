e9cb90876ac09623904b8060c4eeaf13f1c6e6f8
rampremverma108 <vermarp221@gmail.com>
2026-06-22 23:21:12 +0530

    String this way will behave equal

--- string/stringCompare.java
+++ string/stringCompare.java
@@ -6,16 +6,22 @@ public static void main (String args[]){
     String s2 = "Tony";
     String s3 = new String("Tony");
 
-    if(s1 == s2){
-        System.out.println("String are Equal");
-    }else{
-        System.out.println("String are not Equal");
-    }
+    // if(s1 == s2){
+    //     System.out.println("String are Equal");
+    // }else{
+    //     System.out.println("String are not Equal");
+    // }
 
-    if(s1 == s3){
-        System.out.println("String are Equal");
+    // if(s1 == s3){
+    //     System.out.println("String are Equal");
+    // }else{
+    //     System.out.println("String are not Equal");
+    // }
+
+    if(s1.equals(s3)){
+        System.out.print("String are Equal");
     }else{
-        System.out.println("String are not Equal");
+        System.out.print("String are not Equal");
     }
 }
 }
