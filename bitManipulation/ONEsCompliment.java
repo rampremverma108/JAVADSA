361a2e83260f54e86c540312bcabe2f770288bc2
rampremverma108 <vermarp221@gmail.com>
2026-06-26 23:01:26 +0530

    use of one's operator with msb concept

---
+++ bitManipulation/ONEsCompliment.java
@@ -0,0 +1,6 @@
+public class ONEsoperator{
+    public static void main(String args[]){
+        System.out.println(~5);
+    }
+}
+//OUTPUT: excepted 2 but we get -6 because of the MSB(most significant bit)
