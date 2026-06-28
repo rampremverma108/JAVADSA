3c603c0076ac0bd2505dcb11d0ac8c725bb25315
rampremverma108 <vermarp221@gmail.com>
2026-06-28 22:38:43 +0530

    clear any ith bit

---
+++ bitManipulation/clear_ithBit.java
@@ -0,0 +1,11 @@
+public class clear_ithBit{
+    public static int clearIthBit(int n, int i){
+        int bitMask = ~(1<<i);
+        return n &bitMask;
+    }
+    public static void main(String args[]){
+        System.out.println(clearIthBit(10, 1));
+    }
+}
+
+//output: 8
