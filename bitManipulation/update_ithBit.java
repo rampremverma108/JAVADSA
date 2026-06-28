0ef38f150295823ef6c268822c67751462297301
rampremverma108 <vermarp221@gmail.com>
2026-06-28 23:34:14 +0530

    update IthBit

---
+++ bitManipulation/update_ithBit.java
@@ -0,0 +1,26 @@
+public class update_ithBit{
+
+public static int clearIthBit(int n, int i){
+        int bitMask = ~(1<<i);
+        return n &bitMask;
+    }
+
+    public static int setIthBit(int n, int i ){
+    int bitMask = 1<<i;
+    return n | bitMask;
+}
+//out fn
+    public static int updateIthBit(int n, int i, int newBit){
+        //  if(newBit == 0){
+        //     return clearIthBit(n, i);
+        //  }else{
+        //     return setIthBit(n, i);
+        //  }
+         n = clearIthBit(n, i);
+        int BitMask = newBit<<i;
+        return n | BitMask;
+    }
+    public static void main(String args[]){
+        System.out.println(updateIthBit(10, 2, 1));
+    }
+}
