f364c9b005858d4b988aebeea35e31d680308359
rampremverma108 <vermarp221@gmail.com>
2026-06-29 22:53:47 +0530

    clear bit in Range

---
+++ bitManipulation/clrRangeBit.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class clrRangeBit{
+    public static int clearBitRange(int n, int i, int j){
+        int a =((~0)<<(j+1));
+        int b = (1<<i)-1;
+        int bitMask = a | b;
+        return n & bitMask;
+    }
+    public static void main(String args[]){
+        System.out.println(clearBitRange(10, 2, 4));
+    }
+}
+
