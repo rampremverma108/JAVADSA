908eeef0e47eba5c0869a3d5c65123da4d9921d9
rampremverma108 <vermarp221@gmail.com>
2026-06-30 23:38:57 +0530

    add 1 to n, through NOT operator

---
+++ bitManipulation/addition.java
@@ -0,0 +1,10 @@
+public class ADD{
+    public static int Addition(){
+        int a = 10;
+        int sum = -~a;
+        return sum;
+    }
+    public static void main(String args[]){
+        System.out.println(Addition());
+    }
+}
