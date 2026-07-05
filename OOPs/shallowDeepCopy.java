41aebffc3121a37b4c1bf6badc56777a613275c6
rampremverma108 <vermarp221@gmail.com>
2026-07-05 23:39:09 +0530

    shallow and deep copy examples

---
+++ OOPs/shallowDeepCopy.java
@@ -0,0 +1,54 @@
+public class shallowDeepCopy{
+    public static void main(String args[]){
+        Student s1 = new Student();
+        s1.name = "Ram";
+        s1.roll = 123;
+        s1.password = "abcd";
+        s1.marks[0] = 100;
+        s1.marks[1] = 90;
+        s1.marks[2] = 80;
+
+    // s1 ka data s2 mein copy kiya (Deep Copy)
+        Student s2 = new Student(s1);
+        s2.password = "xyz";
+        s1.marks[2] = 100;
+        for(int i =0; i<3; i++){
+            System.out.println(s2.marks[i]);
+        }
+    }
+}
+
+class Student{
+    String name;
+    int roll;
+    String password;
+    int marks[];
+
+// FIX 1: Non-parameterized constructor taaki s1 bante hi array initialize ho jaye
+    Student() {
+        marks = new int[3]; 
+    }
+
+//Shallow copy constructor
+
+// Student(Student s1){
+//     marks = new int[3];
+//     this.name = s1.name;
+//     this.roll = s1.roll;
+//     this.marks = s1.marks;
+//}
+
+//deep copy constructor
+
+Student(Student s1){
+    marks = new int [3];
+    this.name = s1.name;
+    this.roll = s1.roll;
+    for(int i = 0; i<marks.length; i++){
+        this.marks[i] =s1.marks[i];
+    }
+}
+}
+
+
+// Output yaha pr value change nhi hui 100, 90, 80 due to deep copy
