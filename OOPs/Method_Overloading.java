7183e9efe8d8caf472985d720639eb03e69c263a
rampremverma108 <vermarp221@gmail.com>
2026-07-07 00:52:28 +0530

    Method Overloading POLYMORPHISM

---
+++ OOPs/Method_Overloading.java
@@ -0,0 +1,22 @@
+public class MethodOverloading{
+    public static void main(String args[]){
+        Calculator calc = new Calculator();
+        System.out.println(calc.sum(1,2));
+        System.out.println(calc.sum((float)1.2, (float)2.4));
+        System.out.println(calc.sum(1,2,5));
+    }
+}
+
+class Calculator {
+    int  sum(int a, int b){
+        return a + b;
+    }
+
+    float sum(float a, float b){
+        return a + b;
+    }
+
+    float sum(int a, int b, int c){
+        return a + b + c;
+    }
+}
