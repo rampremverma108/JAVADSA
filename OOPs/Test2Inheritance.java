4733b1ebcd3d03bb522a03521a138954f21b6f91
rampremverma108 <vermarp221@gmail.com>
2026-07-10 20:09:59 +0530

    inheritance problem b/w base and drive class

---
+++ OOPs/Test2Inheritance.java
@@ -0,0 +1,21 @@
+public class Test2Inheritance{
+    public static void main(String args[]){
+        //Vehical obj1 = new Car();
+        //obj1.print1();   //Error only access Base class bcoz reffernce Vehical
+
+        Vehical obj2 = new Vehical();
+        obj2.print();
+    }
+}
+
+class Vehical{
+    void print(){
+        System.out.println("Base class (Vehical)");
+    }
+}
+
+class Car extends Vehical{
+    void print1(){
+        System.out.println("Drived class (Car)");
+    }
+}
