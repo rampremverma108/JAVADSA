f30e77db169251cac1644c2918d07e0ff126ace6
rampremverma108 <vermarp221@gmail.com>
2026-07-10 20:39:18 +0530

    Inheritance confusion b/w Base and Drived Class

--- OOPs/Test2Inheritance.java
+++ OOPs/Test2Inheritance.java
@@ -1,7 +1,7 @@
 public class Test2Inheritance{
     public static void main(String args[]){
         //Vehical obj1 = new Car();
-        //obj1.print1();   //Error only access Base class bcoz reffernce Vehical
+        //obj1.print1();   //Error only access Base class bcoz reffernce Vehical 
 
         Vehical obj2 = new Vehical();
         obj2.print();
