26ffb8b6103a4c48bb7bf9a73b2bcc7bfe6c0762
rampremverma108 <vermarp221@gmail.com>
2026-07-10 19:29:16 +0530

    inhertance logical congusion

---
+++ OOPs/TestInheritance.java
@@ -0,0 +1,20 @@
+public class TestInheritance{
+    public static void main(String args[]){
+        Vehical obj1 = new Car();
+        obj1.print();
+        Vehical obj2 = new Vehical();
+        obj2.print();
+    }
+}
+
+class Vehical{
+    void print(){
+        System.out.println("Base class (Vehical)");
+    }
+}
+
+class Car extends Vehical{
+    void print(){
+        System.out.println("Drived class (Car)");
+    }
+}
