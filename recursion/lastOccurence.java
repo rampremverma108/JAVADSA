431c1b1f5872da06079d67f1ff3801d220217436
rampremverma108 <vermarp221@gmail.com>
2026-07-15 00:05:57 +0530

    fixing problems

--- recursion/lastOccurence.java
+++ recursion/lastOccurence.java
@@ -1,22 +1,18 @@
 public class last_Occurence{
 
-    public static int lastOccur(int arr[], int key, int i){
+    public static int lastOccurence(int arr[], int key, int i){
         if(i == arr.length){
             return -1;
         }
 
-        int isFound = lastOccurence(arr, key, i+1)
+        int isFound = lastOccurence(arr, key, i+1);
         if(isFound == -1 && arr[i] == key){
-            return i;
-        }
-
-        if(arr[i] == key){
             return i;
         }
         return isFound;
     }
     public static void main(String args[]){
     int arr[] = {8,3,6,9,5,10,2,5,3};
-    System.out.println(lastOccur(arr,5, 0));
+    System.out.println(lastOccurence(arr,5, 0));
     }
 }
