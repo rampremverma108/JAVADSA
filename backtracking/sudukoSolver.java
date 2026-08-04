1fd38e1cdb09696eba14c6a715560e9152698a8d
rampremverma108 <vermarp221@gmail.com>
2026-08-04 19:59:55 +0530

    Sudoku final code submission

--- backtracking/sudukoSolver.java
+++ backtracking/sudukoSolver.java
@@ -1,20 +1,74 @@
-public class Sudokuu{
-    public static boolean sudukuSolver (int sudoku [][], int row, int col){
-        //base case
-
-        //recursion
-        int nextRow = ro, nextCol = col+1;
-        if(col+1 == 9){
-            nextRow = row+1;
-            nextCol =0;
+public class SudokuReal {
+    public static boolean isSafe(int sudoku[][], int row, int col, int digit){
+        //column check
+        for(int i=0; i<9; i++){
+            if(sudoku[row][i] == digit){
+                return false;
+            } 
         }
-        for(int digit=1; digit<=; digit++){
-            if(isSafe(sudoku, row, col, digit)){
-                sudoku[row][col] = digit;
-                sudukuSolver(sudoku, nextRow, nextCol);
+
+        // row check
+        for(int i=0; i<9; i++){
+            if(sudoku[i][col] == digit){
+                return false;
             }
         }
+
+        //3x3 grid check
+        int sr = (row/3)*3;
+        int sc = (col/3)*3;
+        for(int i=sr; i<sr+3; i++){
+            for(int j=sc; j<sc+3; j++){
+                if(sudoku[i][j] == digit){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
+
+
+    public static boolean sudukuSolver (int sudoku [][], int row, int col){
+        //base case
+        if(row == 9){
+            return true;
+        }else if(col == 9){
+            return false;
+        }
+
+        //recursion
+        int nextRow = row, nextCol = col+1;
+        if(col+1 == 9){
+            nextRow = row+1;
+            nextCol = 0;
+        }
+
+if(sudoku[row][col] != 0){
+            return sudukuSolver(sudoku, nextRow, nextCol);
+        }
+
+        for(int digit=1; digit<= 9; digit++){
+            if(isSafe(sudoku, row, col, digit)){
+                sudoku[row][col] = digit;
+                if(sudukuSolver(sudoku, nextRow, nextCol)){ //sol exitence
+                    return true;
+                }
+                sudoku[row][col] = 0; //backtrack
+            }
+        }
+        return false;
+    }
+
+public static void printSudoku(int sudoku[][]){
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                System.out.print(sudoku[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
     public static void main(String args[]){
         int sudoku[][] = {{0,0,8,0,0,0,0,0,0},
         {4,9,0,1,5,7,0,0,2},
@@ -25,5 +79,12 @@ public class Sudokuu{
         {0,3,0,0,7,2,0,0,4},
         {0,4,9,0,3,0,0,5,7},
         {8,2,7,0,0,9,0,1,3} };
+
+        if(sudukuSolver(sudoku, 0, 0)){
+            System.out.println("Solution exists");
+            printSudoku(sudoku);
+        }else{
+            System.out.println("Solution does not exist");
         }
     }
+}
