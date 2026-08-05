6729abfd0e5a491f7c4747c843955bcd90f9e2e9
rampremverma108 <vermarp221@gmail.com>
2026-08-05 22:28:25 +0530

    ArrayList representation

---
+++ arrayList/ArrayList_representation.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class ArrayList_representation{
+    public static void main(String[] args){
+    //its same as Classname objectname = new Classname();
+        ArrayList<Integer> list = new ArrayList<>();
+        ArratLiast<String> list2 = new ArrayList<>();
+    }
+}
