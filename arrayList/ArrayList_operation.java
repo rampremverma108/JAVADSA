36d466464e400f1537ae2c575aa58deae9cf6538
rampremverma108 <vermarp221@gmail.com>
2026-08-05 23:09:24 +0530

    array operation add Integer and String

--- arrayList/ArrayList_operation.java
+++ arrayList/ArrayList_operation.java
@@ -5,5 +5,12 @@ public class ArrayList_representation{
     //its same as Classname objectname = new Classname();
         ArrayList<Integer> list = new ArrayList<>();
         ArrayList<String> list2 = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list);
+        list2.add("Hello");
+        list2.add("World");
+        System.out.println(list2);
     }
 }
