3d7d81c3046bedbb554f2e58ff2907acccb6f6af
rampremverma108 <vermarp221@gmail.com>
2026-08-08 22:16:57 +0530

    multidimensional ArrayList

---
+++ arrayList/multiDimensional.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class multiDimensional{
+    public static void main(String args[]){
+        ArrayList<ArrayList<Integer>> mainlist = new ArrayList<>();
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1); list.add(2);
+
+        mainlist.add(list);
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(3); list2.add(4);
+
+        mainlist.add(list2);
+
+        System.out.println(mainlist);
+    }
+}
