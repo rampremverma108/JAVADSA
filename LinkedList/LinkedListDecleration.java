bc0a1ebac84288781ed0679f6e25425a4c9b3140
rampremverma108 <vermarp221@gmail.com>
2026-08-14 22:54:12 +0530

    LinkedList Decleration

---
+++ LinkedList/LinkedListDecleration.java
@@ -0,0 +1,15 @@
+public class LinkedList{
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+    }
+}git
