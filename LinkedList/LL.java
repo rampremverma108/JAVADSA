3a308545cbe0b54b42dd5754f6e548382a15907e
rampremverma108 <vermarp221@gmail.com>
2026-08-16 23:59:31 +0530

    add last

--- LinkedList/LL.java
+++ LinkedList/LL.java
@@ -1,27 +1,44 @@
-class LL{
-    class Node{
+class LL {
+    // 1. List ke start ko track karne ke liye head pointer
+    Node head;
+
+    // 2. Inner Node class (static rakhna behtar rehta hai)
+    static class Node {
         String data;
         Node next;
 
-        Node(String data){ 
+        Node(String data) {
             this.data = data;
             this.next = null;
         }
     }
 
-    //add - firstadd, lastadd 
-    public Node addFirst(String data){
+    // 3. List ke end me naya node add karne ka method
+    public void add(String data) {
         Node newNode = new Node(data);
-       if(head == null){
+        if (head == null) {
             head = newNode;
             return;
         }
-        newNode.next = head;
-        head = newNode;
+
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
     }
 
+    // 4. List ke saare elements print karne ka method
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         LL list = new LL();
         list.add("1");
         list.add("2");
