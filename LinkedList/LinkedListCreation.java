e119cc0c751772475f717d6c5f4d75093d28ef6e
rampremverma108 <vermarp221@gmail.com>
2026-08-17 23:54:48 +0530

    linkedlist creation

---
+++ LinkedList/LinkedListCreation.java
@@ -0,0 +1,31 @@
+class Node {
+    int data;
+    Node next;
+
+    // Constructor
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // 1. Create individual nodes
+        Node node1 = new Node(10);
+        Node node2 = new Node(20);
+        Node node3 = new Node(30);
+
+        // 2. Connect the nodes (Link them)
+        node1.next = node2; // 10 -> 20
+        node2.next = node3; // 20 -> 30
+
+        // 3. Head points to the start of the list
+        Node head = node1;
+
+        // 4. Access data using references
+        System.out.println(head.data);           // Prints: 10
+        System.out.println(head.next.data);      // Prints: 20
+        System.out.println(head.next.next.data); // Prints: 30
+    }
+}
