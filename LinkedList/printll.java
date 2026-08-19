76d3f5c5e82bae5c3b84d682277eca3a37c98fc3
rampremverma108 <vermarp221@gmail.com>
2026-08-19 23:52:00 +0530

    print fn added

---
+++ LinkedList/printll.java
@@ -0,0 +1,47 @@
+public class PrintLL{
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+
+   public static Node head;
+   public static Node tail;     
+
+//methods
+public void addLast(int data){
+    if(head == null){
+        Node newNode = new Node(data);
+        head = tail = newNode;
+        return;
+    }
+    //step -1 create new node
+    Node newNode = new Node(data);
+    //step -2 link new node with tail
+    tail.next = newNode; //link
+    //step -3 update tail
+    tail = newNode;
+}
+
+
+public void print(){
+    Node temp = head;
+    while(temp != null){
+        System.out.print(temp.data + "->");
+        temp = temp.next;
+    }
+    System.out.println("null");
+}
+
+public static void main(String[] args) {
+    PrintLL ll = new PrintLL();
+    ll.addLast(1);
+    ll.addLast(2);
+    ll.print();
+}
+}
