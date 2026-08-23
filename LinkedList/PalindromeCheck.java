278ba21f6f86a908c8a79b09484e2fb507c3a03e
rampremverma108 <vermarp221@gmail.com>
2026-08-23 23:40:35 +0530

    check palindrome added find middle

---
+++ LinkedList/PalindromeCheck.java
@@ -0,0 +1,31 @@
+public class PalindromeCheck {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+  //find middle
+  public Node findMid(Node head){
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+
+    
+
+    public static void main(String args[]){
+
+    }
