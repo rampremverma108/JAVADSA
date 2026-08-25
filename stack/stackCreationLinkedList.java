c471d04ee685eaf317d77b079b68a2b0b4c2a022
rampremverma108 <vermarp221@gmail.com>
2026-08-25 14:40:00 +0530

    operation push peek

--- stack/stackCreationLinkedList.java
+++ stack/stackCreationLinkedList.java
@@ -49,6 +49,13 @@ public class stackCreationLinkedList {
         }
     }
     public static void main(String[] args){
-       
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        while(!s.isEmpty()){
+            System.out.print(s.peek()+" ");
+            s.pop();
+        }
 }
 }
