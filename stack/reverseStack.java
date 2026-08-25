2a588ffb938ddf98ecb0e610beef706fca4b40f7
rampremverma108 <vermarp221@gmail.com>
2026-08-25 19:05:30 +0530

    reverse string in stack

---
+++ stack/reverseStack.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+//reverse string using stack
+public class reverseStack {
+    public static String reverseString(String str) {
+        Stack<Character> stack = new Stack<>();
+
+       int idx = 0;
+        while (idx < str.length()) {
+            stack.push(str.charAt(idx));
+            idx++;
+        }
+
+        StringBuilder reversedStr = new StringBuilder();
+        while (!stack.isEmpty()) {
+            char curr = stack.pop();
+            reversedStr.append(curr);
+        }
+
+        return reversedStr.toString();
+    }
+        public static void main(String[] args) {
+        String str = "Hello, World!";
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + reverseString(str));
+}
+}
