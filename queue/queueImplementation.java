55e8d06e595de0e2585e3305fdf0185fce910107
rampremverma108 <vermarp221@gmail.com>
2026-08-30 23:28:26 +0530

    queue final code peek 1,2,3

--- queue/queueImplementation.java
+++ queue/queueImplementation.java
@@ -1,48 +1,65 @@
 import java.util.*;
 
-public class queueImplementation{
-    static class Queue{
-        static int arr[];
-        static int size;
-        static int rear;
-    }
+public class QueueImplementation {
+    static class Queue {
+        int arr[];
+        int size;
+        int rear;
 
-    Queue(int n ){
-        arr = new int[n];
-        size = n;
-        rear = -1;
-    }
-
-    public static boolean isEmpty(){
-        return rear = -1;
-    }
-    
-public static void add(int data){
-     if(rear = size-1){
-            System.out.println("queue is full");
-            return;
+        // Constructor belongs inside the Queue class
+        Queue(int n) {
+            arr = new int[n];
+            size = n;
+            rear = -1;
         }
 
-        rear = rear +1;
-        arr[rear] = data;
-}
+        public boolean isEmpty() {
+            return rear == -1; 
+        }
 
-public static int remove(){
-    if(isEmpty()){
-        System.out.println("Empty queue");
-        return -1;
+        public void add(int data) {
+            if (rear == size - 1) { 
+                System.out.println("Queue is full");
+                return;
+            }
+
+            rear = rear + 1;
+            arr[rear] = data;
+        }
+
+        public int remove() {
+            if (isEmpty()) {
+                System.out.println("Empty queue");
+                return -1;
+            }
+
+            int front = arr[0];
+            for (int i = 0; i < rear; i++) {
+                arr[i] = arr[i + 1];
+            }
+            rear = rear - 1;
+            return front;
+        }
+
+        public int peek() {
+            if (isEmpty()) {
+                System.out.println("Empty queue");
+                return -1;
+            }
+
+            return arr[0];
+        }
     }
-
-    int front = arr[0];
-    for(int i=0; i< rear; i++){
-        arr[i] = arr[i+1];
-    }
-    rear = rear -1;
-    return front;
-
-}
 
     public static void main(String[] args) {
-       
+        Queue Q = new Queue(5);
+        Q.add(1);
+        Q.add(2);
+        Q.add(3);
+
+        while (!Q.isEmpty()) {
+            System.out.println(Q.peek());
+            Q.remove();
+        }
     }
 }
