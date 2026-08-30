fd966f08371981b7e81ed1947a901fc2469f2f72
rampremverma108 <vermarp221@gmail.com>
2026-08-30 22:23:33 +0530

    queue is full logic

--- queue/queueImplementation.java
+++ queue/queueImplementation.java
@@ -1,9 +1,30 @@
 import java.util.*;
 
 public class queueImplementation{
+    static class Queue{
+        static int arr[];
+        static int size;
+        static int rear;
+    }
 
+    Queue(int n ){
+        arr = new int[n];
+        size = n;
+        rear = -1;
+    }
+
+    public static boolean isEmpty(){
+        return rear = -1;
+    }
     
+public static void add(int data){
+     if(rear = size-1){
+            System.out.println("queue is Full");
+            return;
+        }
+}
+
     public static void main(String[] args) {
-        
+       
     }
 }
